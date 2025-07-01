0bebc655e7cde4190a6d7068dbcc8670260461c2
jonahsanpedro <jonahsanpedro@gmail.com>
2025-07-01 12:51:57 -0700

    Project 13 commit

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const {
   createItem,
   getItems,
-  // comment out, updateItem, keeping for reference
   deleteItem,
   likeItem,
   dislikeItem,
@@ -12,9 +11,6 @@ const {
 router.post("/", createItem);
 
 router.get("/", getItems);
-
-// comment out, keeping for reference
-// router.put("/:itemId", updateItem);
 
 router.delete("/:id", deleteItem);
 
